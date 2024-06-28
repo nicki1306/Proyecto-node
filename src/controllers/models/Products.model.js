66e6b1ef69f5192742f33053972139877273f613
Nicole <nickii.cn@hotmail.com>
2024-06-28 17:30:57 -0300

    avances

---
+++ src/controllers/models/Products.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    thumbnail: { type: String, required: true },
+    category: { type: String, required: true },
+    code: { type: String, required: true, unique: true },
+    status: { type: Boolean, default: true }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
